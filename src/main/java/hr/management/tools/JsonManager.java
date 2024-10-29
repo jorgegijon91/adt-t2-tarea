90961f214f2816b3b7a10833265b471e8c7dec35
jorgegijon91 <jofresme@gmail.com>
2024-10-29 11:46:47 +0100

    Completamos la clase JsonManager y añadimos la dependencia al pom

--- src/main/java/hr/management/tools/JsonManager.java
+++ src/main/java/hr/management/tools/JsonManager.java
@@ -1,4 +1,49 @@
 package hr.management.tools;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hr.management.model.Employee;
+import hr.management.model.Vehicle;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class JsonManager {
+    //Atributo objectMapper
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static boolean saveEmployeesToJson(ArrayList<Employee> employees, String filePath) {
+        try {
+            objectMapper.writeValue(new File(filePath), employees);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public static ArrayList<Employee> loadEmployeesFromJson(String filePath) {
+        try {
+            return objectMapper.readValue(new File(filePath), objectMapper.getTypeFactory().constructCollectionType(ArrayList.class, Employee.class));
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public static boolean saveVehiclesToJson(ArrayList<Vehicle> vehicles, String filePath) {
+        try {
+            objectMapper.writeValue(new File(filePath), vehicles);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+
+    public static ArrayList<Vehicle> loadVehiclesFromJson(String filePath) {
+        try {
+            return objectMapper.readValue(new File(filePath), objectMapper.getTypeFactory().constructCollectionType(ArrayList.class, Vehicle.class));
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
 }
